764fd2938e702cd66a23e1392629a05a7a6e495b
Saranya Vatti <saranya.vatti@gmail.com>
2018-09-14 01:27:57 -0400

    Update find-peak-element.java

--- Medium/find-peak-element.java
+++ Medium/find-peak-element.java
@@ -2,11 +2,13 @@ class Solution {
     public int findPeakElement(int[] nums) {
         if(nums == null || nums.length == 0) return -1;
         if(nums.length == 1) return 0;
-        if(nums[1] < nums[0]) return 0;
-        if(nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
-        for(int i=1;i<nums.length-1;i++) {
-            if(nums[i] > nums[i+1]) return i;
-        }
-        return -1;
+        return helper(nums, 0, nums.length-1);
+        
+    }
+    private int helper(int[] nums, int start, int end) {
+        if(start == end) return start;
+        if(nums[start+1] < nums[start]) return start;
+        if(nums[end] > nums[end-1]) return end;
+        return helper(nums, start+1, end-1);
     }
 }
