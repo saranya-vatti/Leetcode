70ce5cf5b598e041607b2f027f1a614e37e7977c
Saranya Vatti <saranya.vatti@gmail.com>
2018-09-24 21:38:27 -0400

    Update find-smallest-letter-greater-than-target.java

--- Easy/find-smallest-letter-greater-than-target.java
+++ Easy/find-smallest-letter-greater-than-target.java
@@ -1,13 +1,12 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        HashSet<Character> set = new HashSet<>();
-        for(int i=0;i<letters.length;i++) {
-            set.add(letters[i]);
-        }
-        for(char i=(char)(target+1); i<= 'z';i++) {
-            if(set.contains(i)) {
-                return i;
-            }
+        int lo = 0, hi = letters.length-2;
+        while(lo<=hi) {
+            //System.out.println("lo = " + lo + " and hi = " + hi);
+            int mid=(lo+hi)/2;
+            if(letters[mid] <= target && letters[mid+1] > target) return letters[mid+1];
+            if(letters[mid] <= target) lo = mid+1;
+            else hi = mid-1;
         }
         return letters[0];
     }
