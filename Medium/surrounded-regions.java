76988e1471f9e1f4efb4ae1ae9b20fcfcd8f82bf
Saranya Vatti <saranya.vatti@gmail.com>
2018-09-27 22:27:29 -0400

    Create surrounded-regions.java

---
+++ Medium/surrounded-regions.java
@@ -0,0 +1,70 @@
+class Solution {
+    public void solve(char[][] grid) {
+        if(grid == null || grid.length == 0) return;
+        int row=0;
+        int col=0;
+        for(col=0;col<grid[0].length;col++) {
+            if(grid[row][col] == 'O') {
+                dfs(grid, row, col, '+');
+            }
+    }
+        col=grid[0].length-1;
+        for(row=0;row<grid.length;row++) {
+            if(grid[row][col] == 'O') {
+                dfs(grid, row, col, '+');
+            }
+    }
+        row=grid.length-1;
+        for(col=0;col<grid[0].length;col++) {
+            if(grid[row][col] == 'O') {
+                dfs(grid, row, col, '+');
+            }
+    }
+        col=0;
+        for(row=0;row<grid.length;row++) {
+            if(grid[row][col] == 'O') {
+                dfs(grid, row, col, '+');
+            }
+    }
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[0].length;j++) {
+                if(grid[i][j] == 'O') {
+                    if(i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1) {
+                        dfs(grid, i, j, '+');
+                    }
+                }
+            }
+        }
+        /*for(int i=0;i<grid.length;i++) {
+            System.out.println(Arrays.toString(grid[i]));
+        }
+        System.out.println();*/
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[0].length;j++) {
+                if(grid[i][j] == 'O') {
+                    dfs(grid, i, j, 'X');
+                }
+            }
+        }
+        /*for(int i=0;i<grid.length;i++) {
+            System.out.println(Arrays.toString(grid[i]));
+        }*/
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[0].length;j++) {
+                if(grid[i][j] == '+') {
+                    grid[i][j] = 'O';
+                }
+            }
+        }
+    }
+    private void dfs(char[][] grid, int row, int col, char changeTo) {
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == changeTo || grid[row][col] == 'X') {
+            return;
+        }
+        grid[row][col] = changeTo;
+        dfs(grid, row+1, col, changeTo);
+        dfs(grid, row, col+1, changeTo);
+        dfs(grid, row-1, col, changeTo);
+        dfs(grid, row, col-1, changeTo);
+    }
+}
