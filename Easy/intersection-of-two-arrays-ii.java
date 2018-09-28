2e482dca8cc1c003338fe93eee4707b3406dd4b8
Saranya Vatti <saranya.vatti@gmail.com>
2018-09-28 05:40:14 -0400

    Update intersection-of-two-arrays-ii.java

--- Easy/intersection-of-two-arrays-ii.java
+++ Easy/intersection-of-two-arrays-ii.java
@@ -1,30 +1,22 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
-        for(int i=0;i<nums1.length;i++) {
-            if(!map1.containsKey(nums1[i])) {
-                map1.put(nums1[i],0);
-            }
-            map1.put(nums1[i], map1.get(nums1[i])+1);
-        }
-        for(int i=0;i<nums2.length;i++) {
-            if(!map2.containsKey(nums2[i])) {
-                map2.put(nums2[i],0);
-            }
-            map2.put(nums2[i], map2.get(nums2[i])+1);
-        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0, j=0;
         ArrayList<Integer> list = new ArrayList<>();
-        for(int key:map1.keySet()) {
-            if(map2.containsKey(key)) {
-                int repeat = Math.min(map1.get(key), map2.get(key));
-                for(int i=0;i<repeat;i++) {
-                    list.add(key);
-                }
+        while(i<nums1.length && j<nums2.length) {
+            if(nums1[i]==nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if(nums1[i]<nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         int[] arr = new int[list.size()];
-        for(int i=0;i<arr.length;i++) {
+        for(i=0;i<arr.length;i++) {
             arr[i] = list.get(i);
         }
         return arr;
