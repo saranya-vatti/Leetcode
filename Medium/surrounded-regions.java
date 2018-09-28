d9cf55a81179de77940c8e6423da43ad9621164a
Saranya Vatti <saranya.vatti@gmail.com>
2018-09-27 22:28:23 -0400

    Update surrounded-regions.java

--- Medium/surrounded-regions.java
+++ Medium/surrounded-regions.java
@@ -1,31 +1,5 @@
 class Solution {
     public void solve(char[][] grid) {
-        if(grid == null || grid.length == 0) return;
-        int row=0;
-        int col=0;
-        for(col=0;col<grid[0].length;col++) {
-            if(grid[row][col] == 'O') {
-                dfs(grid, row, col, '+');
-            }
-    }
-        col=grid[0].length-1;
-        for(row=0;row<grid.length;row++) {
-            if(grid[row][col] == 'O') {
-                dfs(grid, row, col, '+');
-            }
-    }
-        row=grid.length-1;
-        for(col=0;col<grid[0].length;col++) {
-            if(grid[row][col] == 'O') {
-                dfs(grid, row, col, '+');
-            }
-    }
-        col=0;
-        for(row=0;row<grid.length;row++) {
-            if(grid[row][col] == 'O') {
-                dfs(grid, row, col, '+');
-            }
-    }
         for(int i=0;i<grid.length;i++) {
             for(int j=0;j<grid[0].length;j++) {
                 if(grid[i][j] == 'O') {
