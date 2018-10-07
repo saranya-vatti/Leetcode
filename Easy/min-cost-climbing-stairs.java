7654030f4fbdc962bda22e29b7ed7cb1ed00b306
Saranya Vatti <saranya.vatti@gmail.com>
2018-10-07 04:39:35 -0400

    Update min-cost-climbing-stairs.java

--- Easy/min-cost-climbing-stairs.java
+++ Easy/min-cost-climbing-stairs.java
@@ -1,19 +1,8 @@
 class Solution {
-    int[] memoization;
     public int minCostClimbingStairs(int[] cost) {
-        memoization = new int[cost.length];
-        for(int i=0;i<cost.length;i++) {
-            memoization[i] = -1;
+        for(int i=2;i<cost.length;i++) {
+            cost[i] = cost[i] + Math.min(cost[i-1], cost[i-2]);
         }
-        return Math.min(helper(cost, 0), helper(cost, 1));
-    }
-    private int helper(int[] cost, int startIndex) {
-        if(startIndex >= cost.length) return 0;
-        if(memoization[startIndex] != -1) return memoization[startIndex];
-        int ans;
-        if(startIndex == cost.length-1) ans = cost[startIndex];
-        else ans = cost[startIndex] + Math.min(helper(cost, startIndex+1), helper(cost, startIndex+2));
-        memoization[startIndex] = ans;
-        return ans;
+        return Math.min(cost[cost.length-1], cost[cost.length-2]);
     }
 }
