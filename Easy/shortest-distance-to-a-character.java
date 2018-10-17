66a3145e4a3d99d0bc3361578eabe1fd97664530
Saranya Vatti <saranya.vatti@gmail.com>
2018-10-16 22:37:53 -0400

    Update shortest-distance-to-a-character.java

--- Easy/shortest-distance-to-a-character.java
+++ Easy/shortest-distance-to-a-character.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] shortestToChar(String S, char C) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0;i<S.length();i++) {
-            if(S.charAt(i) == C) {
-                list.add(i);
-            }
-        }
         int[] arr = new int[S.length()];
-        for(int i=0;i<arr.length;i++) {
-            arr[i] = Integer.MAX_VALUE;
-        }
-        for(int index = 0;index<S.length();index++) {
-            for(int i=0;i<list.size();i++) {
-                if(Math.abs(list.get(i)-index) < arr[index]) {
-                    arr[index] = Math.abs(list.get(i)-index);
-                }
-            }
+        for(int i=0;i<S.length();i++) {
+            String a = S.substring(0, i);
+            int aIndex = a.lastIndexOf(C);
+            int aDist, bDist;
+            if(aIndex == -1) aDist = Integer.MAX_VALUE;
+            else aDist = i-aIndex;
+            String b = S.substring(i);
+            int bIndex = i+b.indexOf(C);
+            if(bIndex == i-1) bDist = Integer.MAX_VALUE;
+            else bDist = bIndex-i;
+            arr[i] = Math.min(aDist, bDist);
         }
         return arr;
     }
