7525093a3823af7729cc6854455237f6331f7a52
Saranya Vatti <saranya.vatti@gmail.com>
2018-12-09 12:02:09 -0500

    Update maximal-square.java

--- Medium/maximal-square.java
+++ Medium/maximal-square.java
@@ -12,18 +12,10 @@ class Solution {
         for(int i=1;i<=rows;i++) {
             for(int j=1;j<=cols;j++) {
                 if(matrix[i-1][j-1] == '1') {
-                    max[i][j] = Math.min(max[i-1][j], max[i][j-1]);
-                    max[i][j] = Math.min(max[i][j], max[i-1][j-1]);
-                    max[i][j]++;
+                    max[i][j] = Math.min(max[i-1][j],Math.min(max[i][j-1], max[i-1][j-1]))+1;
                     if(max[i][j]>result) result = max[i][j];
                 }
             }
-        }
-        for(int i=0;i<rows;i++) {
-            for(int j=0;j<cols;j++) {
-                System.out.print(max[i][j] + " ");
-            }
-            System.out.println();
         }
         return result*result;
     }
